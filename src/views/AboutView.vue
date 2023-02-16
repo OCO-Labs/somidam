2713b8c99d54830da77fe97b14c68f09827dd35d
yemson <lier@kakao.com>
2023-02-16 21:54:46 +0900

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>메뉴</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <div>여기다가 메인페이지 만들면 되용 하트하트 뿅뿅ㅎㅎ</div>
+</template>
+
+<!-- css style을 따로 정의할 땐 꼭 scoped 로!! -->
+<!-- https://v2.tailwindcss.com/docs/container -->
+<!-- tailwindcss는 그냥 이렇게 class안에 정의하면 됩니다. tailwindcss docs에서 원하는 class 명을 찾고 div class에 갖다 붙히깅 하트하트 뾰ㅗㅇ뿅 -->
