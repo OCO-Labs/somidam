27f44ce50f71fa428f566b26a8676e475fd7abd4
DevSeon <kangsun61@gmail.com>
2023-02-26 02:58:58 +0900

    added example size

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,81 @@
-<script setup>
-
-
-
-
-</script>
+<script setup></script>
 
 <template>
-  
-  <div class="flex flex-col" >
-    <img src="../assets/somidam_logo.svg" class="max-h-96 bg-black"/>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-screen w-full bg-black mx-auto"
+    />
   </div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-  <div></div>
-
-
-  
-
-  
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <img src="../assets/somidam_logo.svg" alt="Somidam Logo" width="110" />
+      <ul
+        class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white"
+      >
+        <li>
+          <a
+            href="#"
+            class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
+            aria-current="page"
+            >브랜드</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
+            >메뉴</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
+            >창업안내</a
+          >
+        </li>
+        <li>
+          <a
+            href="#"
+            class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
+            >가맹안내</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-96 w-full bg-black mx-auto"
+    />
+  </div>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-96 w-full bg-black mx-auto"
+    />
+  </div>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-96 w-full bg-black mx-auto"
+    />
+  </div>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-96 w-full bg-black mx-auto"
+    />
+  </div>
+  <div>
+    <img
+      src="../assets/somidam_logo.svg"
+      class="object-contain h-96 w-full bg-black mx-auto"
+    />
+  </div>
 </template>
-
-
 
 <!-- css style을 따로 정의할 땐 꼭 scoped 로!! -->
 <!-- https://v2.tailwindcss.com/docs/container -->
