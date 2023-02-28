c2a51a18e8ce81640bdd0206cc3c88d5bd6c909e
goldtg <kangsun61@gmail.com>
2023-02-28 19:49:26 +0900

    style: no code change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,15 @@
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-screen w-full bg-black mx-auto"
-    />
+    >
   </div>
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <img src="../assets/somidam_logo.svg" alt="Somidam Logo" width="110" />
+      <img
+        src="../assets/somidam_logo.svg"
+        alt="Somidam Logo"
+        width="110"
+      >
       <ul
         class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white"
       >
@@ -18,29 +22,25 @@
             href="#"
             class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
             aria-current="page"
-            >브랜드</a
-          >
+          >브랜드</a>
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
-            >메뉴</a
-          >
+          >메뉴</a>
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
-            >창업안내</a
-          >
+          >창업안내</a>
         </li>
         <li>
           <a
             href="#"
             class="block py-2 pl-3 pr-4 font-bold text-lg text-somidam rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0"
-            >가맹안내</a
-          >
+          >가맹안내</a>
         </li>
       </ul>
     </div>
@@ -49,31 +49,31 @@
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-96 w-full bg-black mx-auto"
-    />
+    >
   </div>
   <div>
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-96 w-full bg-black mx-auto"
-    />
+    >
   </div>
   <div>
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-96 w-full bg-black mx-auto"
-    />
+    >
   </div>
   <div>
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-96 w-full bg-black mx-auto"
-    />
+    >
   </div>
   <div>
     <img
       src="../assets/somidam_logo.svg"
       class="object-contain h-96 w-full bg-black mx-auto"
-    />
+    >
   </div>
 </template>
 
