bd3abd6511cd45988aec38340d240f5914f96552
goldtg <kangsun61@gmail.com>
2023-03-01 01:31:42 +0900

    feat: 스크롤 네비게이션 디자인 추가

---
+++ src/components/NavScroll.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'NavScroll',
+};
+</script>
+<template>
+  <nav>
+    <div
+      class="flex flex-col items-end pr-8 space-y-4
+    "
+    >
+      <img
+        class="navmenu "
+        src="../assets/logo.svg"
+        alt=""
+      >
+      <img
+        class="navmenu"
+        src="../assets/logo.svg"
+        alt=""
+      >
+      <img
+        class="navmenu"
+        src="../assets/logo.svg"
+        alt=""
+      >
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navmenu {
+width: 70px;
+height: 70px;
+
+}
+</style>
