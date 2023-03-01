a917332d4145cd1270281815c69c243d128b12c8
yemson <lier@kakao.com>
2023-03-01 23:03:48 +0900

    feat: 상단 디자인 추가

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,6 +1,43 @@
 <template>
-  <div class="about">
-    <h1>메뉴</h1>
+  <div class="container mx-auto">
+    <div class="flex flex-col items-center">
+      <img
+        class="mt-32"
+        src="../assets/logo.svg"
+        alt="Somidam Logo"
+        width="130"
+        height="130"
+      >
+      <div class="text-somidam text-8xl font-bold mt-4">
+        MENU
+      </div>
+      <div class="text-somidam text-md">
+        찾으시는 메뉴를 클릭해서 확인해보세요
+      </div>
+    </div>
+    <div class="flex items-center justify-center space-x-6 mt-28">
+      <div class="text-somidam text-5xl font-bold">
+        DESSERT MENU
+      </div>
+      <hr class="w-2/3 h-0.5 bg-somidam">
+    </div>
+    <!-- <div class="grid grid-cols-4 gap-4">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img
+          class="w-full"
+          src="/img/card-top.jpg"
+          alt="Sunset in the mountains"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            소미담 모듬떡 세트
+          </div>
+          <p class="text-gray-700 text-base">
+            16,800원
+          </p>
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
