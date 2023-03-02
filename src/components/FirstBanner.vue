2364b8be4febff7fc77b06d39efdef586d578908
goldtg <kangsun61@gmail.com>
2023-03-03 01:49:04 +0900

    style: 공백, 세미콜론 삭제

--- src/components/FirstBanner.vue
+++ src/components/FirstBanner.vue
@@ -1,9 +1,7 @@
 <script>
 export default {
   name: 'FirstBanner',
-};
-
-
+}
 </script>
 
 <template>
--- src/components/SecondBanner.vue
+++ src/components/SecondBanner.vue
@@ -1,7 +1,7 @@
 <script>
 export default{
   name: 'SecondBanner',
-};
+}
 </script>
 <template>
   <div>
