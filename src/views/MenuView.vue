07a21e0fe84c71627b11028857930278baa9b936
yemson <lier@kakao.com>
2023-03-02 21:30:37 +0900

    feat: 디저트 카드 컴포넌트

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,3 +1,14 @@
+<script>
+import DessertMenuCard from '../components/DessertMenuCard.vue'
+
+export default ({
+  name: 'MenuView',
+  components: {
+    DessertMenuCard
+  }
+})
+</script>
+
 <template>
   <div class="container mx-auto">
     <div class="flex flex-col items-center">
@@ -5,13 +16,13 @@
         class="mt-32"
         src="../assets/logo.svg"
         alt="Somidam Logo"
-        width="130"
-        height="130"
+        width="150"
+        height="150"
       >
-      <div class="text-somidam text-8xl font-bold mt-4">
+      <div class="text-somidam text-9xl font-bold mt-3">
         MENU
       </div>
-      <div class="text-somidam text-md">
+      <div class="text-somidam text-2xl">
         찾으시는 메뉴를 클릭해서 확인해보세요
       </div>
     </div>
@@ -19,27 +30,27 @@
       <div class="text-somidam text-5xl font-bold">
         DESSERT MENU
       </div>
-      <hr class="w-2/3 h-0.5 bg-somidam">
+      <hr class="2xl:w-[52rem] xl:w-[49rem] lg:w-[38rem] hidden md:block md:w-1/2 h-0.5 bg-somidam">
     </div>
-    <!-- <div class="grid grid-cols-4 gap-4">
-      <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          class="w-full"
-          src="/img/card-top.jpg"
-          alt="Sunset in the mountains"
-        >
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">
-            소미담 모듬떡 세트
-          </div>
-          <p class="text-gray-700 text-base">
-            16,800원
-          </p>
-        </div>
-      </div>
-    </div> -->
+    <!-- 후우.. -->
+    <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-4 2xl:mx-40 xl:mx-14 lg:mx-4 mt-10 mb-32">
+      <DessertMenuCard
+        dessert-name="소미담 모듬떡 세트"
+        dessert-price="16,800원"
+        dessert-image="1.jpg"
+      />
+      <DessertMenuCard
+        dessert-name="자색 고구마 크림떡"
+        dessert-price="16,800원"
+        dessert-image="2.jpg"
+      />
+      <DessertMenuCard
+        dessert-name="카스테라 옥수수 크림떡"
+        dessert-price="16,800원"
+        dessert-image="3.jpg"
+      />
+    </div>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
