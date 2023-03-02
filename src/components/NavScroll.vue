5c88fef4aebaa61a5e0afcbc443401c0bfe11bf2
goldtg <kangsun61@gmail.com>
2023-03-03 01:53:43 +0900

    style: css삭제, tailwind적용

--- src/components/NavScroll.vue
+++ src/components/NavScroll.vue
@@ -9,31 +9,20 @@ export default {
   <nav>
     <div class="flex flex-col items-end space-y-4 pb-8 fixed top-96 left-0 right-10 ">
       <img
-        class="navmenu bg-[#946045]"
+        class="rounded-full h-20 w-20 bg-[#946045]"
         src="../assets/bars2.svg"
         alt=""
       >
       <img
-        class="navmenu bg-[#946045]"
+        class="rounded-full h-20 w-20 bg-[#946045]"
         src="../assets/bars3.svg"
         alt=""
       >
       <img
-        class="navmenu bg-[#946045]"
+        class="rounded-full h-20 w-20 bg-[#946045]"
         src="../assets/bars4.svg"
         alt=""
       >
     </div>
   </nav>
 </template>
-
-<style scoped>
-.navmenu {
-width: 70px;
-height: 70px;
-border-radius: 50%;
-
-}
-
-
-</style>
