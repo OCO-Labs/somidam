e0ab59f6f328e0c5718ebf674f55e63fcc4810c0
yemson <lier@kakao.com>
2023-03-03 18:01:39 +0900

    feat: re

---
+++ src/components/CopyFooter.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="bg-gray-200 h-52 px-28 pt-10">
+    <div class="flex justify-between">
+      <img
+        src="../assets/logo.svg"
+        alt="Somidam Logo"
+        width="180"
+        height="180"
+      >
+      <div class="flex flex-col w-2/3 text-sm self-center">
+        <div class="flex">
+          <div class="font-bold">
+            Compnay/CEO
+          </div>
+          <div class="mx-2">
+            |
+          </div>
+          <div>
+            SMC INTERNATIONAL CO., LTD. / Minchang Shin, Hanna Choi
+          </div>
+        </div>
+        <div class="flex">
+          <div class="font-bold">
+            Address
+          </div>
+          <div class="mx-2">
+            |
+          </div>
+          <div>321, Eonju-ro, Gangnam-gu, Seoul, Republic of Korea [Zip Code: 06226]</div>
+        </div>
+        <div class="flex">
+          <div class="font-bold">
+            Customer Service
+          </div>
+          <div class="mx-2">
+            |
+          </div>
+          <div>1644 - 2927</div>
+        </div>
+        <div class="flex">
+          <div class="font-bold">
+            Registraion Number
+          </div>
+          <div class="mx-2">
+            |
+          </div>
+          <div>719 - 86 - 00092</div>
+        </div>
+      </div>
+      <div class="flex flex-col items-end">
+        <div class="text-2xl font-bold mb-2">
+          FOLLOW US
+        </div>
+        <div class="flex space-x-3">
+          <img
+            src="../assets/sns/facebook.svg"
+            alt="facebook"
+            width="12"
+            height="12"
+          >
+          <img
+            src="../assets/sns/instagram.svg"
+            alt="instagram"
+            width="12"
+            height="12"
+          >
+          <img
+            src="../assets/sns/youtube.svg"
+            alt="youtube"
+            width="12"
+            height="12"
+          >
+          <!-- 네이버 svg 만들어야 함 -->
+          <img
+            src="../assets/sns/naver.svg"
+            alt="naver"
+            width="12"
+            height="12"
+          >
+          <img
+            src="../assets/sns/tiktok.svg"
+            alt="tiktok"
+            width="12"
+            height="12"
+          >
+        </div>
+        <div class="text-2xl font-bold mt-2">
+          CONTACT US
+        </div>
+        <div>
+          email@email.com
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAOS4SOeiPkFkSE-nfEplN0VU_wXQ6Bj6Y",
+    authDomain: "somidam-1cacc.firebaseapp.com",
+    projectId: "somidam-1cacc",
+    storageBucket: "somidam-1cacc.appspot.com",
+    messagingSenderId: "101690661602",
+    appId: "1:101690661602:web:0cb28a0725520129825cbb",
+    measurementId: "G-KHP9VW0GG4"
+}
 
-
+const firebase = initializeApp(firebaseConfig)
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(firebase)
 const app = createApp(App)
 
 app.use(router)
