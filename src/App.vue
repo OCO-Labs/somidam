f9e9022d4b29e15700f305b6c8a2ef75ef2ee886
yemson <lier@kakao.com>
2023-03-03 18:01:16 +0900

    feat: footer 추가 및 analytics 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script>
 import { RouterView } from 'vue-router'
+import CopyFooter from './components/CopyFooter.vue'
 
 export default {
   components: {
     RouterView,
+    CopyFooter,
   }
 }
 </script>
 
 <template>
   <RouterView />
+  <CopyFooter />
 </template>
